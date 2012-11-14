af832b99df0249dded5b0f16020691553e3f3cbf
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2012-11-14 13:45:28 +0100

    Adding link to show/hide the filter module

--- filter-rule-module.js
+++ filter-rule-module.js
@@ -1,14 +1,35 @@
 var FilterRuleModule = function(parentElement, filter) {
   var moduleID = 'ln-filter-rule-module-' + createRandomID();
-  var module = GM_getResourceText('filter-rule-module').replace('@@', moduleID);
-  parentElement.append(module);
 
-  NewFilterRuleForm(moduleID, filter);
-  FilterRuleList(moduleID, filter);
+  parentElement.append('<div class="ln-toggle-filter-rule-module-link"><a href="#">+</a></div>')
+               .find('div.ln-toggle-filter-rule-module-link a')
+               .click(toggleModule);
 
   function createRandomID() {
     var randomValue = (Math.random()).toString();
 
     return randomValue.substring(2, 6);
+  }
+
+  function toggleModule() {
+    var filterRuleModule = $('div#' + moduleID);
+
+    $(this).text($(this).text() === '+' ? '–' : '+');
+
+    if (filterRuleModule.length > 0) {
+      filterRuleModule.toggle();
+    } else {
+      createFilterRuleModule();
+    }
+
+    return false;
+  }
+
+  function createFilterRuleModule() {
+    var module = GM_getResourceText('filter-rule-module').replace('@@', moduleID);
+    parentElement.append(module);
+
+    NewFilterRuleForm(moduleID, filter);
+    FilterRuleList(moduleID, filter);
   }
 }
