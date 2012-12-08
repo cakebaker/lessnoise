e8c060b23552b8fdae71e2442211ffa6e3428001
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2012-12-08 15:27:49 +0100

    Adding MutationObserver fix for Chrome

--- new-tweets-bar-clicker.js
+++ new-tweets-bar-clicker.js
@@ -1,4 +1,8 @@
 var NewTweetsBarClicker = function() {
+  // XXX fix for Chrome
+  if (typeof(MutationObserver) == 'undefined') {
+    MutationObserver = WebKitMutationObserver;
+  }
   var target = document.querySelector('div.stream-container');
   var observer = new MutationObserver(function() { $('div.new-tweets-bar').click(); });
   observer.observe(target, { childList: true });
