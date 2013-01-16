84a935751664b7131278826d80fb09207ed40608
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2013-01-16 08:12:03 +0100

    Updating jQuery to v1.9.0

--- lessnoise.user.js
+++ lessnoise.user.js
@@ -5,7 +5,7 @@
 // @description      LessNoise is a filter for Twitter's timeline
 // @include          https://twitter.com/
 // @include          https://twitter.com/#
-// @require          lib/jquery-1.8.3.min.js
+// @require          lib/jquery-1.9.0.min.js
 // @require          new-tweets-bar-clicker.js
 // @require          timeline.js
 // @require          url-expander.js
