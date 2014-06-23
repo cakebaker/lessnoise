05c5652cf2b0b13712a6a7890b4a53c4f9a6d5a5
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2014-06-23 15:17:16 +0200

    Adding hack to disable Twitter's auto-scrolling

--- new-tweets-bar-clicker.js
+++ new-tweets-bar-clicker.js
@@ -3,7 +3,18 @@ var NewTweetsBarClicker = function() {
   if (typeof(MutationObserver) == 'undefined') {
     MutationObserver = WebKitMutationObserver;
   }
+
+  var clickNewTweetsBar = function() {
+    // XXX disabling Twitter's auto-scrolling to the top by temporarily replacing jQuery's animate() function
+    var animateFn = unsafeWindow.$.fn.animate;
+    unsafeWindow.$.fn.animate = function() {};
+
+    $('div.new-tweets-bar').click();
+
+    unsafeWindow.$.fn.animate = animateFn;
+  }
+
   var target = document.querySelector('div.stream-container');
-  var observer = new MutationObserver(function() { $('div.new-tweets-bar').click(); });
+  var observer = new MutationObserver(clickNewTweetsBar);
   observer.observe(target, { childList: true });
 }
