457c152f954909f35b03805b1fadb14b5dd2e004
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2014-08-25 09:46:45 +0200

    Removing workaround that causes problems in FF 31

--- new-tweets-bar-clicker.js
+++ new-tweets-bar-clicker.js
@@ -5,13 +5,7 @@ var NewTweetsBarClicker = function() {
   }
 
   var clickNewTweetsBar = function() {
-    // XXX disabling Twitter's auto-scrolling to the top by temporarily replacing jQuery's animate() function
-    var animateFn = unsafeWindow.$.fn.animate;
-    unsafeWindow.$.fn.animate = exportFunction(function() {}, unsafeWindow);
-
     $('div.new-tweets-bar').click();
-
-    unsafeWindow.$.fn.animate = exportFunction(animateFn, unsafeWindow);
   }
 
   var target = document.querySelector('div.js-new-items-bar-container');
